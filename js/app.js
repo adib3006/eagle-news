14e46f59170268e8ce62fb200ee1cac8be58921c
adib3006 <ma.adib3006@gmail.com>
2022-09-03 00:22:58 +0600

    categories are succesfully fetching related news data

--- js/app.js
+++ js/app.js
@@ -8,7 +8,28 @@ const loadCategories = () => {
 
 const displayNewsCategories = (categories) => {
     categories.forEach(category => {
-        console.log(category);
+        const categoriesField = document.getElementById('categories-section');
+        const url = `https://openapi.programming-hero.com/api/news/category/${category.category_id}`;
+        //console.log(category);
+        const link = document.createElement('button');
+        link.classList.add('btn', 'btn-light', 'text-secondary', 'text-center');
+        link.innerHTML = `
+        <p class="mb-0" onclick="loadNews('${url}')">${category.category_name}</p>
+        `;
+        categoriesField.appendChild(link);
+    })
+}
+
+const loadNews = (url) => {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNews(data.data))
+        .catch(error => console.log(error))
+}
+
+const displayNews = (newsList) => {
+    newsList.forEach(news => {
+        console.log(news);
     })
 }
 
